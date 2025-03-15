c317b9a0b8ede71eb461ba1b7a944870a65ceedc
luizbaidum <luiz.baidum@hotmail.com.br>
2025-03-15 11:54:35 -0300

    microtime in js files call

--- financeiro/src/Views/Principais/view_bottom.php
+++ financeiro/src/Views/Principais/view_bottom.php
@@ -1,8 +1,8 @@
 <?php use src\Diretorio; ?>
 
-<script src="<?= Diretorio::getBaseUrl() ?>/js/jquery.js"></script>
-<script src="<?= Diretorio::getBaseUrl() ?>/js/bootstrap.bundle.js"></script>
-<script src="<?= Diretorio::getBaseUrl() ?>/scripts/functions.js"></script>
-<script src="<?= Diretorio::getBaseUrl() ?>/scripts/geral.js"></script>
-<script src="<?= Diretorio::getBaseUrl() ?>/scripts/formatations.js"></script>
-<script src="<?= Diretorio::getBaseUrl() ?>/scripts/operations.js"></script>
+<script src="<?= Diretorio::getBaseUrl() ?>/js/jquery.js?<?= round(microtime(true), 0); ?>"></script>
+<script src="<?= Diretorio::getBaseUrl() ?>/js/bootstrap.bundle.js?<?= round(microtime(true), 0); ?>"></script>
+<script src="<?= Diretorio::getBaseUrl() ?>/scripts/functions.js?<?= round(microtime(true), 0); ?>"></script>
+<script src="<?= Diretorio::getBaseUrl() ?>/scripts/geral.js?<?= round(microtime(true), 0); ?>"></script>
+<script src="<?= Diretorio::getBaseUrl() ?>/scripts/formatations.js?<?= round(microtime(true), 0); ?>"></script>
+<script src="<?= Diretorio::getBaseUrl() ?>/scripts/operations.js?<?= round(microtime(true), 0); ?>"></script>
