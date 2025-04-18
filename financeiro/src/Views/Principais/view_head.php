b584b897e63a1d46d0cdd8063057573bb672f965
luizbaidum <luiz.baidum@hotmail.com.br>
2025-04-18 16:17:41 -0300

    viewport meta tag e ajuste tela movimentos

--- financeiro/src/Views/Principais/view_head.php
+++ financeiro/src/Views/Principais/view_head.php
@@ -14,6 +14,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?? $this->empresa; ?></title>
     <link rel="stylesheet" href="<?= Diretorio::getBaseUrl() ?>/css/bootstrap.css">
     <link rel="stylesheet" href="<?= Diretorio::getBaseUrl() ?>/css/custom.css">
