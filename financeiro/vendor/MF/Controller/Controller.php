9073591ea120d1cab32b044445afbc65f8550bb2
luizbaidum <luiz.baidum@hotmail.com.br>
2025-06-15 18:10:49 -0300

    atualização da versão

--- financeiro/vendor/MF/Controller/Controller.php
+++ financeiro/vendor/MF/Controller/Controller.php
@@ -14,7 +14,7 @@ class Controller {
 	public function __construct(
 		public $view = new \stdClass(), /*conteudo, base_interna, route, titulo, text*/
 		public string $empresa = 'Bog Finanças',
-        public string $sys_version = 'v1.5.06.10'
+        public string $sys_version = 'v1.5.1506'
 	) {
 		if (isset($_SERVER)) {
 			$this->index_route = 'http://' . $_SERVER['HTTP_HOST'];
