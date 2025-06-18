7b543ee39a9b868e382c1aaa7c5fefee91ff3c85
luizbaidum <luiz.baidum@hotmail.com.br>
2025-06-17 21:36:44 -0300

    rearranjo do menu

--- financeiro/src/System/Menu.php
+++ financeiro/src/System/Menu.php
@@ -16,37 +16,37 @@ class Menu {
     }
 
     public array $grupos = [
-        'Preferencias', 'Cadastros', 'Consultas'
+        'Cadastros', 'Consultas', 'Preferencias'
     ];
 
     public array $titles = [
-        'Preferencias' => [
-            'Preferencias'
-        ],
         'Cadastros' => [
             'Categorias', 'Movimentos', 'Contas Invest', 'Movimentos Mensais', 'Objetivos', 'Orçamento', 'Orçamento do Realizado', 'Movimento entre Investimentos', 'Proprietarios'
         ],
         'Consultas' => [
             'Indicadores', 'Lista Contas Invest', 'Extrato Contas Invest', 'Orçamento', 'Evolução Rendimentos'
+        ],
+        'Preferencias' => [
+            'Preferencias'
         ]
     ];
 
     public array $routes = [
-        'Preferencias' => [
-            'preferencias'
-        ],
         'Cadastros' => [
             'categorias', 'movimentos', 'investimentos', 'movimentos_mensais_index', 'objetivos', 'orcamento', 'orcamento_do_realizado', 'investimentos_movimentar', 'proprietarios'
         ],
         'Consultas' => [
             'indicadores_index', 'contas_investimentos_index', 'extrato_investimentos', 'orcamento_index', 'evolucao_rendimentos'
+        ],
+        'Preferencias' => [
+            'preferencias'
         ]
     ];
 
     public array $niveis = [
-        'Preferencias' => [],
         'Cadastros'    => [],
-        'Consultas'    => []
+        'Consultas'    => [],
+        'Preferencias' => []
     ];
 }
 ?>
