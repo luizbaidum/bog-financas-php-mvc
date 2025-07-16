b58cf2407838c094c76b7eb1a695cbf42aaef177
luizbaidum <luiz.baidum@hotmail.com.br>
2025-07-16 20:54:37 -0300

    Correção idCategoria mov entre aplicação

--- financeiro/src/Routes.php
+++ financeiro/src/Routes.php
@@ -28,7 +28,7 @@ class Routes extends Bootstrap {
         $routes[] = array('route' => '/cad_investimentos', 'controller' => 'InvestimentosController', 'action' => 'cadastrarInvestimentos');
         $routes[] = array('route' => '/objetivos', 'controller' => 'InvestimentosController', 'action' => 'objetivos');
         $routes[] = array('route' => '/cad_objetivos', 'controller' => 'InvestimentosController', 'action' => 'cadastrarObjetivos');
-        $routes[] = array('route' => '/investimentos_movimentar', 'controller' => 'InvestimentosController', 'action' => 'movimentarInvestimentos');
+        $routes[] = array('route' => '/investimentos-movimentar', 'controller' => 'InvestimentosController', 'action' => 'movimentarInvestimentos');
 
         $routes[] = array('route' => '/exibir_resultado', 'controller' => 'MovimentosController', 'action' => 'exibirResultados');
         $routes[] = array('route' => '/movimentos', 'controller' => 'MovimentosController', 'action' => 'index');
